39a1f25d81f0a07629ffd226a8cc3652a13da03e
github username <github email address>
2017-12-26 15:49:26 -0600

    adding pngs to blackjack page

--- casino/client/src/components/Blackjack.js
+++ casino/client/src/components/Blackjack.js
@@ -3,14 +3,18 @@ import React from 'react';
 class Blackjack extends React.Component {
 
   handleClick = () => {
-    
+
   }
 
   render() {
     return (
       <div>
         <h1>Blackjack</h1>
-        <button onClick={this.handleClick}>Deal Hand</button>
+        <button onClick={this.handleClick} width="50" height="50">Play</button><br /><br />
+        <img src={require('../cards/red_back.png')} width="130" height="200"/>
+        <img src={require('../cards/red_back.png')} width="130" height="200"/><br /><br />
+        <img src={require('../cards/red_back.png')} width="130" height="200"/>
+        <img src={require('../cards/red_back.png')} width="130" height="200"/>
       </div>
     );
   }
