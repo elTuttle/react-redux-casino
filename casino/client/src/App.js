a6991f80ac17189809f270d1a98f955a58348df8
github username <github email address>
2017-12-26 14:01:09 -0600

    adding routes to App

--- casino/client/src/App.js
+++ casino/client/src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 import logo from './logo.svg';
+import NavBar from './components/NavBar'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <NavBar />
+          <Route exact path='/' render={Home} />
+          <Route exact path='/actors' render={Blackjack} />
+          <Route exact path='/movies' render={About} />
+        </div>
+      </Router>
     );
   }
 }
