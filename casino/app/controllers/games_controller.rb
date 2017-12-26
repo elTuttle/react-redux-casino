f2fe1fb5ee3be421b963ee817255018c54f87fcd
github username <github email address>
2017-12-26 14:52:09 -0600

    creating json page for new game

--- casino/app/controllers/games_controller.rb
+++ casino/app/controllers/games_controller.rb
@@ -1,2 +1,27 @@
 class GamesController < ApplicationController
+
+  def create
+
+    loop do
+      loop do
+        player_hand = [rand(52),rand(52)]
+        break if player_hand[0] != player_hand[1]
+      end
+
+      loop do
+        dealer_hand = [rand(52),rand(52)]
+        break if dealer_hand[0] != dealer_hand[1]
+      end
+
+      break if dealer_hand[0] != player_hand [0] && dealer_hand[0] != player_hand[1] && dealer_hand[1] != player_hand [0] && dealer_hand[1] != player_hand[1]
+    end
+
+    @game = Game.new(player_hand: player_hand, dealer_hand: dealer_hand)
+
+    if @game.save
+      render json: @game, status: 201
+    end
+
+  end
+
 end
--- casino/app/models/game.rb
+++ casino/app/models/game.rb
@@ -1,2 +1,5 @@
 class Game < ApplicationRecord
+
+  
+
 end
